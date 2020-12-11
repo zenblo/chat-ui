e6d2510ece19544ac595a30400003b4891cbaa6e
zenblo <zenblo@163.com>
2020-12-11 13:15:35 +0800

    fix: 修改 storybook 样式

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import React from "react";
 import { addDecorator, addParameters } from "@storybook/react";
 import { ThemeProvider } from "styled-components";
 import theme from "theme";
+import "story.css";
 
 addDecorator((storyFn) => (
   <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
