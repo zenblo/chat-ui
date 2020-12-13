64800671928672a4c7a5146941d1edd34642163d
zenblo <zenblo@163.com>
2020-12-13 19:17:26 +0800

    docs: 使用模板生成 Input 组件

---
+++ src/components/Input/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StyledInput from "./style";
+
+function Input({children,...rest}) {
+  return (
+    <StyledInput {...rest}>
+      {children}
+    </StyledInput>
+  );
+}
+
+Input.propTypes = {
+  children: PropTypes.any
+};
+
+export default Input;
---
+++ src/components/Input/input.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Input from ".";
+
+export default {
+  title: "Input",
+  component: Input
+};
+
+export const Default = () => <Input>默认</Input>;
