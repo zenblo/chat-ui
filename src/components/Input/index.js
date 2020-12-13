d028045da0d31db7cc0ce17984f9503c5bab490f
zenblo <zenblo@163.com>
2020-12-13 19:39:41 +0800

    feat: 实现搜索框组件 Input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,17 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
-import StyledInput from "./style";
+import StyledInput, { InputContainer, Prefix, Suffix } from "./style";
+import Icon from "components/Icon";
 
-function Input({children,...rest}) {
+import { ReactComponent as SearchIcon } from "assets/icons/search.svg";
+import { useTheme } from "styled-components";
+
+function Input({ placeholder = "请输入内容...", prefix, suffix, ...rest }) {
   return (
-    <StyledInput {...rest}>
-      {children}
-    </StyledInput>
+    <InputContainer {...rest}>
+      {prefix && <Prefix>{prefix}</Prefix>}
+      <StyledInput placeholder={placeholder} />
+      {suffix && <Suffix>{suffix}</Suffix>}
+    </InputContainer>
   );
 }
 
+function Search({ placeholder = "请输入搜索内容...", ...rest }) {
+  const theme = useTheme();
+  return (
+    <Input
+      placeholder={placeholder}
+      prefix={
+        <Icon icon={SearchIcon} color={theme.gray3} width={18} height={18} />
+      }
+      {...rest}
+    />
+  );
+}
+
+Input.Search = Search;
+
 Input.propTypes = {
-  children: PropTypes.any
+  placeholder: PropTypes.string,
+  prefix: PropTypes.any,
+  suffix: PropTypes.any,
 };
 
 export default Input;
--- src/components/Input/input.stories.js
+++ src/components/Input/input.stories.js
@@ -1,9 +1,22 @@
 import React from "react";
 import Input from ".";
+import Icon from "components/Icon";
+
+import { ReactComponent as ClipIcon } from "assets/icons/clip.svg";
+import { ReactComponent as SmileIcon } from "assets/icons/smile.svg";
 
 export default {
-  title: "Input",
-  component: Input
+  title: "UI 组件/Input",
+  component: Input,
 };
 
-export const Default = () => <Input>默认</Input>;
+export const Default = () => <Input />;
+
+export const Search = () => <Input.Search />;
+
+export const WithAffix = () => (
+  <Input
+    prefix={<Icon icon={ClipIcon} color="#cccccc" />}
+    suffix={<Icon icon={SmileIcon} color="#cccccc" />}
+  />
+);
