b1770226a813d9153ac0d61acb94fdd06cb2ab55
zenblo <zenblo@163.com>
2020-12-14 14:46:48 +0800

    feat: 实现标题组件 Heading

--- src/components/Heading/heading.stories.js
+++ src/components/Heading/heading.stories.js
@@ -2,8 +2,13 @@ import React from "react";
 import Heading from ".";
 
 export default {
-  title: "Heading",
-  component: Heading
+  title: "排版/Heading",
+  component: Heading,
 };
 
-export const Default = () => <Heading>默认</Heading>;
+export const H1 = () => <Heading level={1}>这是标题1</Heading>;
+export const H2 = () => <Heading level={2}>这是标题2</Heading>;
+export const H3 = () => <Heading level={3}>这是标题3</Heading>;
+export const H4 = () => <Heading level={4}>这是标题4</Heading>;
+export const H5 = () => <Heading level={5}>这是标题5</Heading>;
+export const H6 = () => <Heading level={6}>这是标题6</Heading>;
--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import StyledHeading from "./style";
 
-function Heading({children,...rest}) {
+function Heading({ children, level, ...rest }) {
   return (
-    <StyledHeading {...rest}>
+    <StyledHeading as={`h${level}`} {...rest}>
       {children}
     </StyledHeading>
   );
 }
 
 Heading.propTypes = {
-  children: PropTypes.any
+  children: PropTypes.any,
+  level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
 };
 
 export default Heading;
