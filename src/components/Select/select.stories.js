268942e1516475418db1ba0e2c3916f3bb4f6f7b
zenblo <zenblo@163.com>
2020-12-15 20:23:46 +0800

    feat: 实现下拉选项组件 Select

--- src/components/Select/select.stories.js
+++ src/components/Select/select.stories.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Select from ".";
+import Option from "components/Option";
 
 export default {
-  title: "Select",
-  component: Select
+  title: "UI 组件/Input/Select",
+  component: Select,
 };
 
-export const Default = () => <Select>默认</Select>;
+export const Default = () => (
+  <Select>
+    <Option>最新消息优先</Option>
+    <Option>在线好友优先</Option>
+  </Select>
+);
