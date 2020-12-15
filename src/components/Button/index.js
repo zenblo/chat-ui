2f5e0fd83fa78570ee3b18c95227f7d90ed4f889
zenblo <zenblo@163.com>
2020-12-15 20:24:29 +0800

    feat: 实现按钮组件 Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,16 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 import StyledButton from "./style";
 
-function Button({children,...rest}) {
+function Button({
+  children,
+  type = "primary",
+  shape = "circle",
+  size = "30px",
+  bgColor,
+  ...rest
+}) {
   return (
-    <StyledButton {...rest}>
+    <StyledButton
+      type={type}
+      shape={shape}
+      size={size}
+      bgColor={bgColor}
+      {...rest}
+    >
       {children}
     </StyledButton>
   );
 }
 
 Button.propTypes = {
-  children: PropTypes.any
+  children: PropTypes.any,
+  type: PropTypes.oneOf(["primary"]),
+  shape: PropTypes.oneOf(["circle", "rect"]),
+  size: PropTypes.string,
 };
 
 export default Button;
