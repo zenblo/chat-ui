cfc18d1f489d4ae60d5a51a6dafb9b4226287c6a
zenblo <zenblo@163.com>
2020-12-16 11:15:14 +0800

    fix: 清除命名错误 MessagrList 组件

--- src/components/MessagrList/messagrList.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MessagrList from ".";
-
-export default {
-  title: "MessagrList",
-  component: MessagrList
-};
-
-export const Default = () => <MessagrList>默认</MessagrList>;
--- src/components/MessagrList/style.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-const StyledMessagrList = styled.div``;
-
-export default StyledMessagrList;
