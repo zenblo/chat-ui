27677261dae19e447f0291aebdac61c7d8168087
zenblo <zenblo@163.com>
2020-12-17 09:45:11 +0800

    docs: 使用模板生成 TitleBar 组件

---
+++ src/components/TitleBar/style.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const StyledTitleBar = styled.div``;
+
+export default StyledTitleBar;
---
+++ src/components/TitleBar/titleBar.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import TitleBar from ".";
+
+export default {
+  title: "TitleBar",
+  component: TitleBar
+};
+
+export const Default = () => <TitleBar>默认</TitleBar>;
