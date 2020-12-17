b2b20c1c5c857508bdd5dbe9b7abc84510656528
zenblo <zenblo@163.com>
2020-12-17 10:59:36 +0800

    feat: 实现会话气泡组件 ChatBubble

--- src/components/ChatBubble/chatBubble.stories.js
+++ src/components/ChatBubble/chatBubble.stories.js
@@ -1,9 +1,31 @@
 import React from "react";
 import ChatBubble from ".";
+import VoiceMessage from "components/VoiceMessage";
 
 export default {
-  title: "ChatBubble",
-  component: ChatBubble
+  title: "UI 组件/ChatBubble",
+  component: ChatBubble,
+  decorators: [(storyFn) => <div style={{ padding: "24px" }}>{storyFn()}</div>],
 };
 
-export const Default = () => <ChatBubble>默认</ChatBubble>;
+export const FromOthers = () => (
+  <ChatBubble time="昨天 下午14：26">这是一条其它人发送的聊天消息</ChatBubble>
+);
+
+export const Mine = () => (
+  <ChatBubble type="mine" time="昨天 下午16：30">
+    这是一条我自己发送的聊天消息
+  </ChatBubble>
+);
+
+export const VoiceMessageType = () => (
+  <ChatBubble time="昨天 下午18：30">
+    <VoiceMessage time="01:24" />
+  </ChatBubble>
+);
+
+export const VoiceMessageTypeMine = () => (
+  <ChatBubble type="mine" time="昨天 下午18：30">
+    <VoiceMessage type="mine" time="01:24" />
+  </ChatBubble>
+);
--- src/components/ChatBubble/index.js
+++ src/components/ChatBubble/index.js
@@ -1,17 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
-import StyledChatBubble from "./style";
+import StyledChatBubble, {
+  Bubble,
+  BubbleTip,
+  Time,
+  MessageText,
+} from "./style";
 
-function ChatBubble({children,...rest}) {
+import { ReactComponent as BubbleTipIcon } from "assets/icons/bubbleTip.svg";
+
+function ChatBubble({ children, type, time, ...rest }) {
   return (
-    <StyledChatBubble {...rest}>
-      {children}
+    <StyledChatBubble type={type} {...rest}>
+      <Bubble>
+        <BubbleTip icon={BubbleTipIcon} width={40} height={28} color="white" />
+        <MessageText>{children}</MessageText>
+      </Bubble>
+      <Time>{time}</Time>
     </StyledChatBubble>
   );
 }
 
 ChatBubble.propTypes = {
-  children: PropTypes.any
+  children: PropTypes.any,
+  type: PropTypes.oneOf(["mine"]),
+  time: PropTypes.string,
 };
 
 export default ChatBubble;
