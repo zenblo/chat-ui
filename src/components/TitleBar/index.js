b8125695177735a0b568abdb274591669f6d67d2
zenblo <zenblo@163.com>
2020-12-17 10:58:28 +0800

    feat: 实现标题栏组件 TitleBar

--- src/components/TitleBar/index.js
+++ src/components/TitleBar/index.js
@@ -1,17 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
-import StyledTitleBar from "./style";
+import StyledTitleBar, { Actions, Title } from "./style";
 
-function TitleBar({children,...rest}) {
+import face from "assets/images/face-male-3.jpg";
+
+import { ReactComponent as Call } from "assets/icons/call.svg";
+import { ReactComponent as Camera } from "assets/icons/camera.svg";
+import { ReactComponent as Options } from "assets/icons/options.svg";
+import Avatar from "components/Avatar";
+import Paragraph from "components/Paragraph";
+import Text from "components/Text";
+import Icon from "components/Icon";
+
+function TitleBar({ children, ...rest }) {
   return (
     <StyledTitleBar {...rest}>
-      {children}
+      <Avatar status="offline" src={face} />
+      <Title>
+        <Paragraph size="large">慕容天宇</Paragraph>
+        <Paragraph type="secondary">
+          <Text>离线</Text>
+          <Text>· 最后阅读：3小时前</Text>
+        </Paragraph>
+      </Title>
+      <Actions>
+        <Icon opacity={0.3} icon={Call} />
+        <Icon opacity={0.3} icon={Camera} />
+        <Icon opacity={0.3} icon={Options} />
+      </Actions>
     </StyledTitleBar>
   );
 }
 
 TitleBar.propTypes = {
-  children: PropTypes.any
+  children: PropTypes.any,
 };
 
 export default TitleBar;
--- src/components/TitleBar/titleBar.stories.js
+++ src/components/TitleBar/titleBar.stories.js
@@ -2,8 +2,8 @@ import React from "react";
 import TitleBar from ".";
 
 export default {
-  title: "TitleBar",
-  component: TitleBar
+  title: "UI 组件/TitleBar",
+  component: TitleBar,
 };
 
-export const Default = () => <TitleBar>默认</TitleBar>;
+export const Default = () => <TitleBar />;
