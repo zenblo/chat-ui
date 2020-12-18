e5be2ef51a503a5f0d9ebc3bd88717c3e40c3bae
zenblo <zenblo@163.com>
2020-12-18 09:34:52 +0800

    docs: 使用模板生成 Emoji 组件

---
+++ src/components/Emoji/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StyledEmoji from "./style";
+
+function Emoji({children,...rest}) {
+  return (
+    <StyledEmoji {...rest}>
+      {children}
+    </StyledEmoji>
+  );
+}
+
+Emoji.propTypes = {
+  children: PropTypes.any
+};
+
+export default Emoji;
