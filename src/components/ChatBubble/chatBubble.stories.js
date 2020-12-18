42a43b2d0e570bcaa96578b898c125ba3aeb42a9
zenblo <zenblo@163.com>
2020-12-18 10:00:36 +0800

    fix: 测试使用 Emoji 会话 ChatBubble

--- src/components/ChatBubble/chatBubble.stories.js
+++ src/components/ChatBubble/chatBubble.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ChatBubble from ".";
 import VoiceMessage from "components/VoiceMessage";
+import Emoji from "components/Emoji";
 
 export default {
   title: "UI 组件/ChatBubble",
@@ -12,9 +13,10 @@ export const FromOthers = () => (
   <ChatBubble time="昨天 下午14：26">这是一条其它人发送的聊天消息</ChatBubble>
 );
 
+/* eslint-disable jsx-a11y/accessible-emoji */
 export const Mine = () => (
   <ChatBubble type="mine" time="昨天 下午16：30">
-    这是一条我自己发送的聊天消息
+    这是一条我自己发送的聊天消息<Emoji label="smile">😄</Emoji>
   </ChatBubble>
 );
 
