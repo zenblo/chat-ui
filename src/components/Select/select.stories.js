a6ee38e90380e2dd080f255fa8a1ecd77663efae
zenblo <zenblo@163.com>
2020-12-22 13:52:57 +0800

    fix: 添加表单组件样式 Select

--- src/components/Select/select.stories.js
+++ src/components/Select/select.stories.js
@@ -13,3 +13,12 @@ export const Default = () => (
     <Option>在线好友优先</Option>
   </Select>
 );
+
+export const FormSelect = () => {
+  return (
+    <Select type="form">
+      <Option>北京市</Option>
+      <Option>河北省</Option>
+    </Select>
+  );
+};
