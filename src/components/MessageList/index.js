6cd3b28d92de8610713f42407a9dc9a0d9ec6382
zenblo <zenblo@163.com>
2020-12-26 19:33:22 +0800

    fix: 设置消息列表动画效果

--- src/components/MessageList/index.js
+++ src/components/MessageList/index.js
@@ -12,48 +12,39 @@ import Input from "components/Input";
 import MessageCard from "components/MessageCard";
 
 import face1 from "assets/images/face-male-1.jpg";
+import FilterList from "components/FilterList";
+import { useTrail, animated } from "react-spring";
+import useStaggeredList from "hooks/useStaggeredList";
 
 function MessageList({ children, ...rest }) {
+  const trailAnimes = useStaggeredList(6);
+
   return (
     <StyledMessageList {...rest}>
-      <Input.Search />
-      <ChatFilter />
-      <ChatList>
-        {[1, 2, 3, 4, 5, 6].map((_, index) => (
-          <MessageCard
-            key={index}
-            active={index === 3}
-            replied={index % 3 === 0}
-            avatarSrc={face1}
-            name="李铭浩"
-            avatarStatus="online"
-            statusText="在线"
-            time="3 小时之前"
-            message="即使爬到最高的山上，一次也只能脚踏实地地"
-            unreadCount={2}
-          />
-        ))}
-      </ChatList>
+      <FilterList
+        options={["最新消息优先", "在线好友优先"]}
+        actionLabel="创建会话"
+      >
+        <ChatList>
+          {[1, 2, 3, 4, 5, 6].map((_, index) => (
+            <animated.div key={index} style={trailAnimes[index]}>
+              <MessageCard
+                key={index}
+                active={index === 3}
+                replied={index % 3 === 0}
+                avatarSrc={face1}
+                name="李铭浩"
+                avatarStatus="online"
+                statusText="在线"
+                time="3 小时之前"
+                message="即使爬到最高的山上，一次也只能脚踏实地地"
+                unreadCount={2}
+              />
+            </animated.div>
+          ))}
+        </ChatList>
+      </FilterList>
     </StyledMessageList>
-  );
-}
-
-function ChatFilter() {
-  return (
-    <Filter style={{ padding: "20px 0" }}>
-      <Filter.Filters label="列表排序">
-        <Select>
-          <Option>最新消息优先</Option>
-          <Option>在线好友优先</Option>
-        </Select>
-      </Filter.Filters>
-
-      <Filter.Action label="创建会话">
-        <Button>
-          <Icon icon={Plus} width={12} height={12} />
-        </Button>
-      </Filter.Action>
-    </Filter>
   );
 }
 
